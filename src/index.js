c7f7433900e3c3cd9182da85c53ab5f039d2d1f0
Sam Buters <sambuters@gmail.com>
2019-04-02 21:32:49 -0300

    Adding API Request and localhost message
    Co-authored-by: Eduardo Lima<eduardolima.df@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const https = require('https');
+
+let data = '';
+
+var lat = 13.0;
+var lon = 34.0;
+
+https.get('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid=652aee6b1beb9ead3681837980b21671', (resp) => {
+  
+	resp.on('data', (chunk) => {
+		data += chunk;
+	});
+
+}).on("error", (err) => {
+	console.log("Error: " + err.message);
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.json({'Hello': 'World'});
-  console.log('LOG');
-})
+	var json_data = JSON.parse(data);
+ 	res.json(json_data);
+});
 
 app.listen(3000);
