0e85c7f95b4fd177ac75b7547d3b37b5e796142d
Micaella <micaella2212@gmail.com>
2019-04-13 00:46:58 -0300

    Changing names os variableschanging name of variables
    Co-authored-by: Sam Buters <sambuters@gmail.com>

--- src/requests/requestSimulation.js
+++ src/requests/requestSimulation.js
@@ -5,27 +5,27 @@ const router = express.Router();
 
 router.get('/simulation', async (req, res) => {
   const { query: { place: target } } = req;
-  let data = '';
-  let JsonData = '';
+  let simulationdata = '';
+  let SimulationJsonData = '';
   const apiKey = process.env.API_KEY;
-  const localData = '{"lat":-15.7934036,"lng":-47.8823172}';
-  const localJsonData = JSON.parse(localData);
+  const slocalData = '{"lat":-15.7934036,"lng":-47.8823172}';
+  const slocalJsonData = JSON.parse(slocalData);
   let lati = '';
   let lon = '';
   if (target === 'brasilia') {
-    lati = localJsonData.lat;
-    lon = localJsonData.lng;
+    lati = slocalJsonData.lat;
+    lon = slocalJsonData.lng;
   } else {
     lati = 'undefined';
     lon = 'undefined';
   }
   https.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${lon}&appid=${apiKey}`, (response) => {
     response.on('data', (chunk) => {
-      data += chunk;
+      simulationdata += chunk;
     });
     response.on('end', () => {
-      JsonData = JSON.parse(data);
-      res.json(JsonData);
+      SimulationJsonData = JSON.parse(simulationdata);
+      res.json(SimulationJsonData);
     });
   });
 });
