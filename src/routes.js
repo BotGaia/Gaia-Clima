1aed5e2d6bd3814291ebcbe45afff2e7a1cd3f57
Eduardo Lima <eduardolima.df@gmail.com>
2019-05-10 17:19:28 -0300

    Adding request and treatment to forecast
    Co-authored-by: Sam Buters <sambuters@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,28 @@ router.get('/climate', (req, res) => {
   requestWeather.getLocal(req.query.place).then((coordsJson) => {
     requestWeather.getWeather(coordsJson).then((weatherJson) => {
       if (weatherJson.cod === 200) {
-        const weather = new Weather(weatherJson);
+        const weather = new Weather(weatherJson, 'weather');
         res.json(weather);
       } else {
         res.json(weatherJson);
+      }
+    });
+  });
+});
+
+router.get('/forecast', (req, res) => {
+  requestWeather.getLocal(req.query.place).then((coordsJson) => {
+    requestWeather.getForecast(coordsJson).then((forecastJson) => {
+      if (forecastJson.cod === '200') {
+        let weatherArray = new Array(40);
+
+        for(counter = 0; counter < 40; counter += 1) {
+          weatherArray[counter] = new Weather(forecastJson.list[counter], 'forecast');
+        }
+
+        res.json(weatherArray);
+      } else {
+        res.json(forecastJson.list);
       }
     });
   });
