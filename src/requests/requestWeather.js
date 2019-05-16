7dc220b9000134cdcd18bf75ba2a07027b3148f9
CalebeRios <calebersmendes@gmail.com>
2019-05-16 16:24:12 -0300

    Changing requisitions from http or https to axios
    Fixing error return from open weather map
    
    Changing to get build from dockerfile
    
    Removing TSL verification
    
    Changing name of file to environment

--- src/requests/requestWeather.js
+++ src/requests/requestWeather.js
@@ -1,67 +1,49 @@
-const https = require('https');
-const http = require('http');
+const axios = require('axios');
 
 module.exports = {
-  getLocal: (target) => {
-    let localData = '';
-    let localJsonData = '';
-
-    return new Promise((resolve) => {
-      http.get(`${global.URL_LOCAL}/local?address=${target}`, (resp) => {
-        resp.on('data', (chunk) => {
-          localData += chunk;
-        });
-
-        resp.on('end', () => {
-          localJsonData = JSON.parse(localData);
-          resolve(localJsonData);
-        });
-      }).on('error', () => {
-        const errorJson = JSON.parse('{"lat":"error","lng":"error"}');
-        resolve(errorJson);
-      });
+  getLocal: target => new Promise((resolve) => {
+    axios.get(`${global.URL_LOCAL}/local`, {
+      params: {
+        address: target,
+      },
+    }).then((response) => {
+      resolve(response.data);
+    }).catch((err) => {
+      resolve(err);
     });
-  },
+  }),
 
   getWeather: (localJson) => {
-    const { lat } = localJson;
-    const { lng } = localJson;
-    let data = '';
-    let JsonData = '';
-    const apiKey = process.env.API_KEY;
+    const params = {
+      lat: localJson.lat,
+      lon: localJson.lng,
+      appid: process.env.API_KEY,
+    };
 
     return new Promise((resolve) => {
-      https.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}`, (resp) => {
-        resp.on('data', (chunk) => {
-          data += chunk;
+      axios.get('https://api.openweathermap.org/data/2.5/weather', { params })
+        .then((response) => {
+          resolve(response.data);
+        }).catch((err) => {
+          resolve(err.response.data);
         });
-
-        resp.on('end', () => {
-          JsonData = JSON.parse(data);
-          resolve(JsonData);
-        });
-      });
     });
   },
 
   getForecast: (localJson) => {
-    const { lat } = localJson;
-    const { lng } = localJson;
-    let data = '';
-    let JsonData = '';
-    const apiKey = process.env.API_KEY;
+    const params = {
+      lat: localJson.lat,
+      lon: localJson.lng,
+      appid: process.env.API_KEY,
+    };
 
     return new Promise((resolve) => {
-      https.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${apiKey}`, (resp) => {
-        resp.on('data', (chunk) => {
-          data += chunk;
+      axios.get('https://api.openweathermap.org/data/2.5/forecast', { params })
+        .then((response) => {
+          resolve(response.data);
+        }).catch((err) => {
+          resolve(err.response.data);
         });
-
-        resp.on('end', () => {
-          JsonData = JSON.parse(data);
-          resolve(JsonData);
-        });
-      });
     });
   },
 };
