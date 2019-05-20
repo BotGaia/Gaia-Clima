ef5ce937aa179be2ed0a80038d00b12186aa5414
TheTaira <luishptaira@gmail.com>
2019-05-20 12:34:00 -0300

    HOTFIX Updating endpoints.ks

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -10,7 +10,59 @@ module.exports = {
             type: 'string',
           },
         ],
-        description: 'Receives a location and return its weather conditions',
+        description: 'Receives a location and returns its weather conditions',
+      },
+      {
+        type: 'GET',
+        endpoint: '/forecast',
+        parameters: [
+          {
+            name: 'place',
+            type: 'string',
+          },
+        ],
+        description: 'Receives a location and returns its weather conditions for 5 days in the future',
+      },
+      {
+        type: 'GET',
+        endpoint: '/climateForecast',
+        parameters: [
+          {
+            name: 'place',
+            type: 'string',
+          },
+          {
+            name: 'day',
+            type: 'integer',
+          },
+          {
+            name: 'month',
+            type: 'integer',
+          },
+          {
+            name: 'year',
+            type: 'integer',
+          },
+          {
+            name: 'hours',
+            type: 'integer',
+          },
+        ],
+        description: 'Receives a location and a date and returns the weather conditions for that date and place',
+      },
+      {
+        type: 'POST',
+        endpoint: '/sportForecast',
+        JSON: {
+            telegramId: 'string',
+            day: 'integer',
+            hours: 'integer',
+            year: 'integer',
+            month: 'integer',
+            sport: 'array',
+            locals: 'array'
+        },
+        description: 'Receives a notification object and a date and returns the favorability of the sport and a weather object for each location.',
       },
       {
         type: 'GET',
